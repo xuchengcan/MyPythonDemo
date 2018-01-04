a8cc2ead00df9c400eee85315d2b4030f28bc8ca
chen <617322616@qq.com>
2018-01-05 00:42:28 +0800

    2018-1-5

--- rename.py
+++ rename.py
@@ -4,43 +4,43 @@ import os
 
 def rename():
 
-	path=os.path.abspath('.')#获得当前工作目录
+    path = os.path.abspath('.')  # 获得当前工作目录
 
-	filelist=os.listdir(path)#该文件夹下所有的文件（包括文件夹）
-	
-	for files in filelist:#遍历所有文件
+    filelist = os.listdir(path)  # 该文件夹下所有的文件（包括文件夹）
 
-		Olddir=os.path.join(path,files);#原来的文件路径
-		
-		if os.path.isdir(Olddir):#如果是文件夹则跳过
-			continue;
+    for files in filelist:  # 遍历所有文件
 
-		filename = os.path.splitext(files)[0];#文件名
+        Olddir = os.path.join(path, files)  # 原来的文件路径
 
-		if "_jiagu_" in filename:
-			filename = "BCZP"
-		elif "_1_" in filename :
-			filename = "BCZP_360"
-		elif "_2_" in filename :
-			filename = "BCZP_sjqq"
-		elif "_3_" in filename :
-			filename = "BCZP_wandoujia"
-		elif "_4_" in filename :
-			filename = "BCZP_baidu"
-		elif "_5_" in filename :
-			filename = "BCZP_xiaomi"
-		elif "_6_" in filename :
-			filename = "BCZP_huawei"
-		elif "_7_" in filename :
-			filename = "BCZP_oppo"
-		elif "_8_" in filename :
-			filename = "BCZP_vivo"
+        if os.path.isdir(Olddir):  # 如果是文件夹则跳过
+            continue
 
-		filetype=os.path.splitext(files)[1];#文件扩展名
+        filename = os.path.splitext(files)[0]  # 文件名
 
-		Newdir=os.path.join(path,filename+filetype);#新的文件路径
+        if "_jiagu_" in filename:
+            filename = "BCZP"
+        elif "_1_" in filename:
+            filename = "BCZP_360"
+        elif "_2_" in filename:
+            filename = "BCZP_sjqq"
+        elif "_3_" in filename:
+            filename = "BCZP_wandoujia"
+        elif "_4_" in filename:
+            filename = "BCZP_baidu"
+        elif "_5_" in filename:
+            filename = "BCZP_xiaomi"
+        elif "_6_" in filename:
+            filename = "BCZP_huawei"
+        elif "_7_" in filename:
+            filename = "BCZP_oppo"
+        elif "_8_" in filename:
+            filename = "BCZP_vivo"
 
-		os.rename(Olddir,Newdir);#重命名
+        filetype = os.path.splitext(files)[1]  # 文件扩展名
 
-rename();
+        Newdir = os.path.join(path, filename + filetype)  # 新的文件路径
 
+        os.rename(Olddir, Newdir)  # 重命名
+
+
+rename()
--- sent_email.py
+++ sent_email.py
@@ -6,16 +6,17 @@ import email.mime.text
 import time
 
 msg = email.mime.multipart.MIMEMultipart()
-msgFrom = '617322616@qq.com' #从该邮箱发送
-msgTo = '617322616@qq.com' #发送到该邮箱
-smtpSever='smtp.qq.com' # 163邮箱的smtp Sever地址
-smtpPort = '465' #开放的端口
-sqm='xxx'  # 在登录smtp时需要login中的密码应当使用授权码而非账户密码
+msgFrom = '617322616@qq.com'  # 从该邮箱发送
+msgTo = '617322616@qq.com'  # 发送到该邮箱
+smtpSever = 'smtp.qq.com'  # 163邮箱的smtp Sever地址
+smtpPort = '465'  # 开放的端口
+sqm = 'xxx'  # 在登录smtp时需要login中的密码应当使用授权码而非账户密码
 
 msg['from'] = msgFrom
 msg['to'] = msgTo
 
-msg['subject'] =  "Python自动邮件 - " + time.strftime("%Y-%m-%d %X", time.localtime())#获取当前系统时间
+msg['subject'] = "Python自动邮件 - " + \
+    time.strftime("%Y-%m-%d %X", time.localtime())  # 获取当前系统时间
 content = '''
 你好:
 这是一封python3发送的邮件
